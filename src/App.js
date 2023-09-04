eeb87772210522a78228801f6a30bdb9d77f5a4f
email <racbernazgul@gmail.com>
2023-09-04 23:54:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Learn React</header>
+    </div>
+  );
+}
+
+export default App;
