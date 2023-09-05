02e4e3b5774a1dd5d50c9767a1b50514ca128f18
email <racbernazgul@gmail.com>
2023-09-06 02:37:11 +0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../../helpers/logo.png"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+          <div className="logo">
+              <img src={logo} alt="horoscope" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
