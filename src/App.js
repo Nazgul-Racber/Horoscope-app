4831258e2a83572ec30147fe50deff1173b26c9c
email <racbernazgul@gmail.com>
2023-09-06 02:10:18 +0300

    header:scss.variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <Header/>
     </div>
   );
 }
